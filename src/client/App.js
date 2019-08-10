7ae88179865d745c4fb473641d52e0aa7273e547
R. Stephen Guerra <rguerra712@yahoo.com>
2019-08-10 16:21:24 -0500

    Fixed minor spacing issue

--- src/client/App.js
+++ src/client/App.js
@@ -4,28 +4,27 @@ import { ConfigConsumer } from '../components/ConfigProvider';
 import ExampleComponent from '../components/ExampleComponent';
 import './App.css';
 
-const App = () => (
-  <ConfigConsumer>
-    {config => (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">
-              Welcome to
-            {config.app.TITLE}
-          </h1>
-        </header>
-        <p className="App-intro">
-            To get started, edit
-          {'App.js'}
-          <code>src/App.js</code>
-          {' '}
-            and save to reload.
-        </p>
-        <ExampleComponent text="Some Text" />
-      </div>
-    )}
-  </ConfigConsumer>
-);
+const App = () => {
+  const getTitle = config => `Welcome to ${config.app.TITLE}`;
+  return (
+    <ConfigConsumer>
+      {config => (
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">
+              {getTitle(config)}
+            </h1>
+          </header>
+          <p className="App-intro">
+              To get started, edit App.js and save to reload.
+          </p>
+          <ExampleComponent text="Some Text" />
+        </div>
+      )
+    }
+    </ConfigConsumer>
+  );
+};
 
 export default App;
